25b253b58b25916ca52ec69d8b44ef6209f43e91
boomyao <boomyao@outlook.com>
2024-10-14 16:38:38 +0800

    feat: auto generate transcript when request

--- transcript_serve.py
+++ transcript_serve.py
@@ -2,13 +2,20 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import os
 import json
+from transcribe import run_steps
 app = Flask(__name__)
 CORS(app)
 
+BASE_DIR = "output"
+
+if not os.path.exists(BASE_DIR):
+    os.makedirs(BASE_DIR, exist_ok=True)
+
 def get_translated_transcript(video_id):
-    output_dir = f"output/{video_id}"
-    if not os.path.exists(output_dir):
-        return None
+    output_dir = f"{BASE_DIR}/{video_id}"
+    if not os.path.exists(output_dir) or not os.path.exists(f"{output_dir}/translated_paragraphs.jsonl"):
+        url = f"https://www.youtube.com/watch?v={video_id}"
+        run_steps(url, output_dir)
     paragraphs = []
     with open(f"{output_dir}/translated_paragraphs.jsonl", "r", encoding="utf-8") as f:
         for line in f:
